649a8cb3ffa064a8eb1d68b7b1baa4c3f07c0094
Tristan <yanggc888@163.com>
2019-06-08 22:11:12 +0800

    docs: first blood

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+const App = () => {
+  return (
+    <>
+      <Header />
+      <div>body</div>
+      <Footer />
+    </>
+  );
+};
+export default App;
