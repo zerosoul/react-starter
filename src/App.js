2d8bb2a6411d107b4243c8faad9440a3bf93a4f9
tristan <yangguochun@ddweilai.com>
2019-09-18 13:48:40 +0800

    feat: add code spliting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React, { lazy, Suspense } from 'react';
+import Loading from './components/Loading';
+const Header = lazy(() => import('./components/Header'));
+const Footer = lazy(() => import('./components/Footer'));
 import styled from 'styled-components';
 const StyledBody = styled.section`
   height: 60vh;
 `;
 const App = () => {
   return (
-    <>
+    <Suspense fallback={<Loading />}>
       <Header />
       <StyledBody>body</StyledBody>
       <Footer />
-    </>
+    </Suspense>
   );
 };
 export default App;
